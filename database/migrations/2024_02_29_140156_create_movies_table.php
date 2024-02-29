f7b75636df93970ddfdc073c7526d7bcf15ff3fc
Hélio M S Olivas <heliomsolivas@gmail.com>
2024-02-29 11:54:22 -0300

    first commit

---
+++ database/migrations/2024_02_29_140156_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('comment_id')->constrained();
+            $table->string('title');
+            $table->integer('overallrate');
+            $table->string('genre');
+            $table->string('description');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
